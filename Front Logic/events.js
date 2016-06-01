1ac30886dfe6ee92117710474e50d93a1853b3ab
JavierCoggiola <javi_cj_98@hotmail.com>
2016-06-01 11:56:52 -0300

    FrontEnd v2.0

---
+++ Front Logic/events.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	$("#imgProfile").click(function(){
+		alert('click en foto de perfil');
+	});
+	$("#inventary").click(function(){
+		alert('click en inventario');
+	});
+	$("#mundialMap").click(function(){
+		alert('click en mapa mundial');
+	});
+    $("#historyChat").click(function(){
+		alert('click en chat global');
+	});
+    $("#peopleTrade").click(function(){
+		alert('click en trade');
+	});
+    $("#peopleParty").click(function(){
+		alert('click en solicitud de party');
+	});
+    $("#peopleAttack").click(function(){
+		alert('click en peopleAttack');
+	});
+    $("#peopleWhisp").click(function(){
+		alert('click en peopleWhisp');
+	});
+    $("#monsterAttack").click(function(){
+		alert('click en monsterAttack');
+	});
+    $("#npcTrade").click(function(){
+		alert('click en npcTrade');
+	});
+    $("#npcAttack").click(function(){
+		alert('click en npcAttack');
+	});
+    $("#itemPickup").click(function(){
+		alert('click en itemPickup');
+	});
+    $("#sendChat").click(function(){
+		alert('click en sendChat');
+	});
+});    
