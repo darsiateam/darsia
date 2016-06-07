4fe89fa46599705701beee58eb223eac201d6d2a
mateosss <mateodemayo@gmail.com>
2016-06-07 19:48:56 -0300

    tutorial php terminado, iniciado tutorial symfony

--- PHP/index.php
+++ PHP/index.php
@@ -4,12 +4,13 @@
 // load and initialize any global libraries
 require_once 'model.php';
 require_once 'controllers.php';
-
 // route the request internally
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if ('/darsia/PHP/index.php' === $uri) {
   list_action();
-} elseif ('/darsia/PHP/index.php/show' === $uri && isset($_GET['id'])) {
+} elseif ('/darsia/PHP/'   === $uri){
+  list_action();
+} elseif ('/darsia/PHP/index.php/show.php' === $uri && isset($_GET['id'])) {
   show_action($_GET['id']);
 } else {
   header('HTTP/1.1 404 Not Found');
